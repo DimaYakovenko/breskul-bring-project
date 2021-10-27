aaf540d6b70f63f8540b88a979c163e877b91126
Misha <mishabeheka@gmail.com>
2021-10-27 15:55:46 +0300

    Created InvalidFileNameException

---
+++ src/main/java/com/bring/project/exception/InvalidFileNameException.java
@@ -0,0 +1,7 @@
+package com.bring.project.exception;
+
+public class InvalidFileNameException extends RuntimeException {
+    public InvalidFileNameException(String message) {
+        super(message);
+    }
+}
