0532f9447136b71401ffc7c43b3ddcbb78a72024
Andrii Bobrov <verydrybeaver@gmail.com>
2021-11-13 11:50:38 +0200

    BR#23 Add Excepiton handling instead of SneakyThrows #23

--- src/test/java/items/service/impl/FakeUserServiceImpl.java
+++ src/test/java/items/service/impl/FakeUserServiceImpl.java
@@ -4,9 +4,11 @@ import com.bringframework.annotation.Inject;
 import com.bringframework.annotation.Item;
 import items.dao.FakeUserRepository;
 import items.service.FakeUserService;
+import lombok.Getter;
 
 @Item
 public class FakeUserServiceImpl implements FakeUserService {
     @Inject
+    @Getter
     private FakeUserRepository fakeUserRepository;
 }
